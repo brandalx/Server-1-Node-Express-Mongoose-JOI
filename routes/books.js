21060bdf8b6a989be5308ff3cec4a7176cdf1df6
Brandon Nolan <brndalx@gmail.com>
2023-02-10 02:19:41 +0200

    feat: CRUD method "GET" limitied with response limit of 4 items

--- routes/books.js
+++ routes/books.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { BooksModel } = require("../models/bookModel");
 router.get("/", async (req, res) => {
   try {
-    let data = await BooksModel.find({});
+    let data = await BooksModel.find({}).limit(4);
     res.json(data);
   } catch (err) {
     console.log(err);
     res.status(502).json({ err });
   }
 });
+
 module.exports = router;
