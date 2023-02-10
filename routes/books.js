861c1ffd5f73f42f7156b1b1d1702eb97143acd5
Brandon Nolan <brndalx@gmail.com>
2023-02-10 02:29:32 +0200

    feat: added query for page listing (.skip); created option for sorting by key or default by date (.sort); created option to reverse sort (.sort)

--- routes/books.js
+++ routes/books.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { BooksModel } = require("../models/bookModel");
 router.get("/", async (req, res) => {
+  let perPage = 4;
+  let page = req.query.page - 1 || 0;
+  let sort = req.query.sort || "DATE_CREATED";
+  let reverse = req.query.reverse == "yes" ? 1 : -1;
   try {
-    let data = await BooksModel.find({}).limit(4);
+    let data = await BooksModel.find({})
+      .limit(4)
+      .skip(page * perPage)
+      .sort({ [sort]: reverse });
     res.json(data);
+
+    //Query request example: for this CRUD "GET" request:  http://localhost:3001/s1books?sort=name&page=1&reverse=yes
   } catch (err) {
     console.log(err);
     res.status(502).json({ err });
